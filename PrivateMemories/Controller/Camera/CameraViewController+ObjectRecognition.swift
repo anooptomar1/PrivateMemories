8e3933ec4743ead30e1d96100a213dd4941edbd9
Krzysztof Babis <babiskrzysztof@gmail.com>
2018-01-19 02:44:43 +0100

    Added static object recognition

--- PrivateMemories/Controller/Camera/CameraViewController+ObjectRecognition.swift
+++ PrivateMemories/Controller/Camera/CameraViewController+ObjectRecognition.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 import CoreLocation
+import Vision
 
 extension CameraViewController {
     
     func recognizeObject() {
+        guard let capturedImage = capturedImageView.image, let image = CIImage(image: capturedImage) else { return }
         
+        recognizedObjectLabel.text = "Analyzing image..."
+            
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            return
+        }
+            
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let bestResult = results.first else {
+                    return
+            }
+                
+            DispatchQueue.main.async { [weak self] in
+                self?.recognizedObjectLabel.text = "\(bestResult.identifier)"
+            }
+        }
+
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
     }
     
     func discardRecognition() {
-        
+        //isRecognizing = false
+        //recognizedObjectView.isHidden = true
     }
     
     func savePhoto() {
@@ -26,6 +54,35 @@ extension CameraViewController {
         let pickedImage = PickedImage(galleryName: galleryName, image: capturedPhoto, location: currentLocation, date: Date(), recognizedObjectDescription: recognizedObjectLabel.text)
         let photoViewModel = PhotoViewModel(from: pickedImage)
         photoViewModel.saveImage(asNewObject: true)
+    }
+    
+    func crop(image: UIImage) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        return newImage
     }
 }
 
