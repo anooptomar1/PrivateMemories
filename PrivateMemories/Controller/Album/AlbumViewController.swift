a99baca64c2722edb55718c2877dcbf037d99424
Krzysztof Babis <babiskrzysztof@gmail.com>
2018-01-20 14:49:59 +0100

    Layout: Album layout changed

--- PrivateMemories/Controller/Album/AlbumViewController.swift
+++ PrivateMemories/Controller/Album/AlbumViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AlbumCollectionViewController.swift
+//  AlbumViewController.swift
 //  PrivateMemories
 //
 //  Created by Krzysztof Babis on 28.11.2017.
@@ -10,12 +10,14 @@ import UIKit
 
 private let reuseIdentifier = "AlbumCollectionViewCell"
 
-class AlbumCollectionViewController: UICollectionViewController {
+class AlbumViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
     fileprivate let albumViewModel = AlbumViewModel()
     fileprivate let gallerySegueIdentifier = "toGallerySegue"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
         setupLayout()
         reloadAlbum()
     }
@@ -38,63 +40,12 @@ class AlbumCollectionViewController: UICollectionViewController {
         presentAddPopup()
     }
     
-    func presentAddPopup() {
-        let alert = UIAlertController(title: "Add gallery", message: "Enter a gallery name", preferredStyle: .alert)
-
-        alert.addTextField { (textField) in
-            textField.placeholder = "Gallery name"
-        }
-        
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
-            let textField = alert?.textFields![0]
-            if let galleryName = textField?.text {
-                if galleryName != "" {
-                    self.albumViewModel.saveGallery(named: galleryName, completion: { (addedSuccessfully) in
-                        if !addedSuccessfully {
-                            print("This gallery name is already taken")
-                        }
-                    })
-                    self.reloadAlbum()
-                }
-            }
-        }))
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: true, completion: nil)
-    }
-
-    
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.albumViewModel.getNumberOfFetchedObjects()
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! AlbumCollectionViewCell
-        let fetchedThumbnail: (name: String, creationDate: String) = self.albumViewModel.getDataOfFetchedObject(at: indexPath)
-        
-        cell.titleLabel.text = fetchedThumbnail.name
-        cell.subtitleLabel.text = fetchedThumbnail.creationDate
-        cell.imageView.image = UIImage(named: "photo")!
-        cell.deleteButton.tag = indexPath.row
-        cell.deleteButton.addTarget(self, action: #selector(deleteButtonPressed(_:)), for: .touchUpInside)
-        
-        return cell
-    }
-    
     @objc func deleteButtonPressed(_ sender: UIButton) {
         let indexPathOfDeletedGallery = IndexPath(row: sender.tag, section: 0)
         albumViewModel.delete(at: indexPathOfDeletedGallery)
         reloadAlbum()
     }
-
+    
     // MARK: Navigation methods
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -106,5 +57,61 @@ class AlbumCollectionViewController: UICollectionViewController {
             print("PUSHING VC")
             galleryVC.selectedGalleryName = selectedGallery.name
         }
+    }
+    
+    func presentAddPopup() {
+        let alert = UIAlertController(title: "Add gallery", message: "Enter a gallery name", preferredStyle: .alert)
+
+        alert.addTextField { (textField) in
+            textField.placeholder = "Gallery name"
+        }
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0]
+            if let galleryName = textField?.text {
+                print("ADDING GALLERY")
+                if galleryName != "" {
+                    self.albumViewModel.saveGallery(named: galleryName, completion: { (addedSuccessfully) in
+                        if !addedSuccessfully {
+                            print("This gallery name is already taken")
+                        } else {
+                            print("SUCCESSFULLY ADDED")
+                        }
+                    })
+                    self.reloadAlbum()
+                }
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+    
+extension AlbumViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("NUMBER OF ITEMS IN SECTION: \(self.albumViewModel.getNumberOfFetchedObjects())")
+        return self.albumViewModel.getNumberOfFetchedObjects()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! AlbumCollectionViewCell
+        print("CELL FOR ROW \(indexPath.row)")
+        let fetchedThumbnail: (name: String, creationDate: String) = self.albumViewModel.getDataOfFetchedObject(at: indexPath)
+        
+        cell.titleLabel.text = fetchedThumbnail.name
+        cell.subtitleLabel.text = fetchedThumbnail.creationDate
+        cell.imageView.image = UIImage(named: "photo")!
+        cell.deleteButton.tag = indexPath.row
+        cell.deleteButton.addTarget(self, action: #selector(deleteButtonPressed(_:)), for: .touchUpInside)
+        
+        return cell
     }
 }
